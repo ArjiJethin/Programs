3c2f73547b6e03c2e955d3dc512a908bac463e5e
ArjiJethin <arjijethin.ac0705@gmail.com>
2024-09-06 21:00:15 +0530

    Minor code snippets adjustments

---
+++ LabProgram.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+int main()
+{
+    int size;
+    
+    printf("Enter the size of the array: ");
+    scanf("%d", &size);
+    
+    char arr[size + 1];
+    //char arr[size];
+
+    getchar(); 
+
+    for (int i = 0; i < size; i++)
+    {
+        printf("Enter element %d: ", i + 1);
+        scanf("%c", &arr[i]);
+        getchar();
+    }
+
+    arr[size] = '\0';
+
+    printf("Array elements: %s\n", arr);
+
+    return 0;
+}
